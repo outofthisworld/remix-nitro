4bb5b44f768d20c0c9c3618864e052eb309a3702
Dale <dale@farpoint.co.nz>
2024-01-07 23:49:09 +1300

    c

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,7 +1,7 @@
 // The root dir for the users project 
 const dir = process.env.INIT_CWD ?? process.cwd();
 
-const config = import(`${dir}/nitro.config.ts`);
+const config = await import(`${dir}/nitro.config.ts`);
 
 //https://nitro.unjs.io/config
 export default config;
